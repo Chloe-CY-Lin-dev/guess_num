de7f70430adca36bfabc70652c5c6d2d4acf1376
Chloe Lin <yg2ne1seven7@gmail.com>
2022-04-18 00:19:28 +0800

    Add new file

---
+++ guess_num.py
@@ -0,0 +1,23 @@
+import random
+start = int(input('請決定隨機數字範圍開始值：'))
+end = int(input('請決定隨機範圍結束值：'))
+
+
+r = random.randint(start,end)
+
+count = 0
+while True:
+    count = count + 1
+    num = int(input('請猜數字：'))
+    # input會把東西存成字串
+    if num == r:
+        print('你猜中了！')
+        print('這次你猜的第', count, '次')
+        break
+    elif num > r:
+        print('猜錯了!比答案大')
+    elif num < r:
+        print('猜錯了!比答案小')
+    print('這次你猜的第', count, '次')
+
+
